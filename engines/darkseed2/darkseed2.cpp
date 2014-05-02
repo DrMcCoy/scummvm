22a320d70105b0173e089bdd4a6823f093103bb4
Matthew Hoops <clone2727@gmail.com>
2014-05-01 23:22:53 -0400

    DARKSEED2: Fix segfaults on failure to start

--- engines/darkseed2/darkseed2.cpp
+++ engines/darkseed2/darkseed2.cpp
@@ -107,6 +107,7 @@ DarkSeed2Engine::DarkSeed2Engine(OSystem *syst, const ADGameDescription *gameDes
 	_inter          = 0;
 	_events         = 0;
 	_macExeResFork  = 0;
+	_midiDriver     = 0;
 
 	_rnd = new Common::RandomSource("darkseed2");
 
@@ -118,8 +119,10 @@ DarkSeed2Engine::DarkSeed2Engine(OSystem *syst, const ADGameDescription *gameDes
 }
 
 DarkSeed2Engine::~DarkSeed2Engine() {
-	_music->stop();
-	_sound->stopAll();
+	if (_music)
+		_music->stop();
+	if (_sound)
+		_sound->stopAll();
 
 	_mixer->stopAll();
 
